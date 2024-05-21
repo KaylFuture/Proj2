0ee11fcd8866dad43b738a4703a8a98d3fe63146
KaylFuture <81624606+KaylFuture@users.noreply.github.com>
2024-05-21 11:46:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+function printValue(val) {
+    if (document.querySelector(".main-information").innerHTML == "0")
+        document.querySelector(".main-information").innerHTML = "";
+    document.querySelector(".main-information").innerHTML += val;
+    if (document.querySelector(".main-information").innerHTML == ".")
+        document.querySelector(".main-information").innerHTML = "0.";
+    if (document.querySelector(".main-information").innerHTML == "00")
+        document.querySelector(".main-information").innerHTML = "0";
+}
+function main() {
+    const buttonsNum = document.querySelectorAll(".number-list > li > button");
+    for (button of buttonsNum) {
+        const buttonText = button.innerHTML;
+        button.onclick = () => printValue(buttonText);
+    }
+
+    const buttonsSym = document.querySelectorAll(".symbols-list-1 > li > button");
+    for (button of buttonsSym) {
+        const buttonText = button.innerHTML;
+        button.onclick = () => printValue(buttonText);
+    }
+    const clearBtn = document.querySelector(".clear-text");
+    const miResult = document.querySelector(".main-information");
+
+    clearBtn.addEventListener('click', function () {
+        miResult.textContent = '';
+        if (miResult.textContent === '') {
+            miResult.textContent = '0';
+        }
+    });
+    if (miResult.textContent === '') {
+        miResult.textContent = '0';
+
+    }
+}
+
+window.onload = main;
