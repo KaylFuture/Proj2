eae8e7d0df1dc0cabb2379b0fca1dbf4bf6de164
KaylFuture <81624606+KaylFuture@users.noreply.github.com>
2024-05-30 23:06:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,157 +1,108 @@
 function printValue(val) {
-  if (
-    document.querySelector(".main-information").innerHTML == "0" &&
-    val != "."
-  ) {
-    document.querySelector(".main-information").innerHTML = "";
-  }
-  if (
-    val == "." &&
-    document.querySelector(".main-information").innerHTML.includes(".")
-  ) {
-    return;
-  }
-  document.querySelector(".main-information").innerHTML += val;
-  console.log(document.querySelector(".main-information"));
-  console.log(val);
-  if (document.querySelector(".main-information").innerHTML == "00")
-    document.querySelector(".main-information").innerHTML = "0";
+    if (document.querySelector(".main-information").innerHTML == "0" && val != ".")
+        document.querySelector(".main-information").innerHTML = "";
+    if (val == "." && document.querySelector(".main-information").innerHTML.includes("."))
+        return;
+    document.querySelector(".main-information").innerHTML += val;
+    if (document.querySelector(".main-information").innerHTML == "00")
+        document.querySelector(".main-information").innerHTML = "0";
 }
 function main() {
-  // let plus = document.querySelector(".plus-text");
-  // let minus = document.querySelector(".minus-text");
-  // let division = document.querySelector(".division-text");
-  // let multiplication = document.querySelector(".multiplication-text");
-  const a = document.querySelector(".history-information").textContent;
-  console.log(a);
-  const buttonsNum = document.querySelectorAll(".number-list > li > button");
-  for (button of buttonsNum) {
-    const buttonText = button.innerHTML;
-    button.onclick = () => printValue(buttonText);
-  }
+    let miHistorylet = document.querySelector(".history-information");
+    // let plus = document.querySelector(".plus-text");
+    // let minus = document.querySelector(".minus-text");
+    // let division = document.querySelector(".division-text");
+    // let multiplication = document.querySelector(".multiplication-text");
 
-  const buttonsSym = document.querySelectorAll(".symbols-list-1 > li > button");
-  for (button of buttonsSym) {
-    const buttonText = button.innerHTML;
-    button.onclick = () => printValue(buttonText);
-  }
-  const clearBtn = document.querySelector(".clear-text");
-  const miResult = document.querySelector(".main-information");
-  const miHistory = document.querySelector(".history-information");
 
-  if (miResult.textContent === "") {
-    miResult.textContent = "0";
-  }
 
-  clearBtn.addEventListener("click", function () {
-    miResult.textContent = "0";
-    miHistory.textContent = "";
-    if (miResult.textContent && miHistory.textContent === "") {
-      miResult.textContent = "0";
-      miHistory.textContent = "";
-    }
-  });
-
-  document.querySelector(".plus-text").addEventListener("click", function () {
-    let a = document.querySelector(".history-information").textContent;
-    if (isOperation(a[a.length - 1])) {
-      a = a.split("");
-      a.pop();
-      document.querySelector(".history-information").textContent = a.join("");
-    }
-    const p1 = document.querySelector(".main-information");
-    const p2 = document.querySelector(".history-information");
-
-    const addValue = p2.textContent
-      ? p1.textContent.split("").reverse().join("")
-      : p1.textContent;
-    p2.textContent += addValue;
-
-    p1.textContent = "0";
-    if (p2.textContent === "+") {
-      p2.textContent = "";
-    }
-  });
-
-  document.querySelector(".minus-text").addEventListener("click", function () {
-    const p1 = document.querySelector(".main-information");
-    const p2 = document.querySelector(".history-information");
-    let a = document.querySelector(".history-information").textContent;
-    if (isOperation(a[a.length - 1])) {
-      a = a.split("");
-      a.pop();
-      document.querySelector(".history-information").textContent = a.join("");
+    const buttonsNum = document.querySelectorAll(".number-list > li > button");
+    for (button of buttonsNum) {
+        const buttonText = button.innerHTML;
+        button.onclick = () => printValue(buttonText);
     }
 
-    let addValue = p2.textContent
-      ? p1.textContent[p1.textContent.length - 1] + p1.textContent
-      : p1.textContent;
-    // addValue.slice(addValue.length - 1, 1); 
-    // потрыбно прибрати ласт символ
-    p2.textContent += addValue;
-    console.log(p1.textContent);
-    p1.textContent = "0";
-  });
+    const buttonsSym = document.querySelectorAll(".symbols-list-1 > li > button");
+    for (button of buttonsSym) {
+        const buttonText = button.innerHTML;
+        button.onclick = () => printValue(buttonText);
+    }
+    const clearBtn = document.querySelector(".clear-text");
+    const miResult = document.querySelector(".main-information");
+    const miHistory = document.querySelector(".history-information");
 
-  document
-    .querySelector(".division-text")
-    .addEventListener("click", function () {
-      const p1 = document.querySelector(".main-information");
-      const p2 = document.querySelector(".history-information");
-      let a = document.querySelector(".history-information").textContent;
-      if (isOperation(a[a.length - 1])) {
-        a = a.split("");
-        a.pop();
-        document.querySelector(".history-information").textContent = a.join("");
-      }
+    if (miResult.textContent === '') {
+        miResult.textContent = '0';
 
-      const addValue = p2.textContent
-        ? p1.textContent.split("").reverse().join("")
-        : p1.textContent;
-      p2.textContent += addValue;
+    }
 
-      p1.textContent = "0";
-      if (p2.textContent === "÷") {
-        p2.textContent = "";
-      }
+    clearBtn.addEventListener('click', function () {
+        miResult.textContent = '0';
+        miHistory.textContent = '';
+        if (miResult.textContent && miHistory.textContent === '') {
+            miResult.textContent = '0';
+            miHistory.textContent = '';
+        }
     });
-  document
-    .querySelector(".multiplication-text")
-    .addEventListener("click", function () {
-      const p1 = document.querySelector(".main-information");
-      const p2 = document.querySelector(".history-information");
-      let a = document.querySelector(".history-information").textContent;
 
-      if (isOperation(a[a.length - 1])) {
-        a = a.split("");
-        a.pop();
-        document.querySelector(".history-information").textContent = a.join("");
-      }
+    document.querySelector(".plus-text").addEventListener('click', function () {
 
-      const addValue = p2.textContent
-        ? p1.textContent.split("").reverse().join("")
-        : p1.textContent;
-      p2.textContent += addValue;
-      p1.textContent = "0";
-      if (p2.textContent === "×") {
-        p2.textContent = "";
-      }
-    });
-  function isOperation(element) {
-    switch (element) {
-      case "÷":
-        return true;
-      case "+":
-        return true;
-      case "-":
-        return true;
-      case "×":
-        return true;
+        const p1 = document.querySelector('.main-information');
+        const p2 = document.querySelector('.history-information');
 
-      default:
-        return false;
-    }
-  }
+        p2.textContent += p1.textContent;
+
+        p1.textContent = '0';
+        if (p2.textContent === "+") {
+            p2.textContent = "";
+        }
+    })
+
+    document.querySelector(".minus-text").addEventListener('click', function () {
+
+        const p1 = document.querySelector('.main-information');
+        const p2 = document.querySelector('.history-information');
+
+        p2.textContent += p1.textContent;
+
+        p1.textContent = '0';
+    })
+
+    document.querySelector(".division-text").addEventListener('click', function () {
+
+        const p1 = document.querySelector('.main-information');
+        const p2 = document.querySelector('.history-information');
+
+        p2.textContent += p1.textContent;
+
+        p1.textContent = '0';
+        if (p2.textContent === "÷") {
+            p2.textContent = "";
+        }
+    })
+    document.querySelector(".multiplication-text").addEventListener('click', function () {
+
+        const p1 = document.querySelector('.main-information');
+        const p2 = document.querySelector('.history-information');
+
+        p2.textContent += p1.textContent;
+
+        p1.textContent = '0';
+        if (p2.textContent === "×") {
+            p2.textContent = "";
+        }
+    })
+    document.querySelector(".equal-text").addEventListener('click', function () {
+        const p1 = document.querySelector('.main-information');
+        const p2 = document.querySelector('.history-information');
+
+        p2.textContent += p1.textContent;
+
+        p1.textContent = '0';
+        if (p2.textContent === "=") {
+            p2.textContent = "";
+        }
+    })
 }
 
 window.onload = main;
